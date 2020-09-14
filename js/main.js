10e1040d17c1da50454312723b6501d60303ba9b
raccon-dev <loealoe@zohomail.eu>
2020-09-14 11:11:44 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const root = document.querySelector('#chart');
+const data = [5, 8, 2, 1, 15, 2, 3, 5, 9, 11, 10, 4, 3, 14, 1, 7, 10, 3, 2, 13];
+
+const main = () => {
+    const table = document.createElement('table');
+    root.appendChild(table)
+}
+main()
